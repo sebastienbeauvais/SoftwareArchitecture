69cae6e680aee7d1f9962f852c87d5d0511d7235
sebastienbeauvais <sbeauvais7@gmail.com>
2024-07-25 07:28:22 -0700

    Added unit test proj

---
+++ UnitTests/UnitTests/Business/BookServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit;
+using BookStore;
+
+namespace UnitTests.Business
+{
+    public class BookServiceTest
+    {
+        [Test]
+        public void GetAllBooks()
+        {
+
+        }
+        [Test]
+        public void AddBook()
+        {
+
+        }
+    }
+}
